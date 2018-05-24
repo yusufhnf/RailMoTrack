c263d5a1f1efa95a783ceb6ac89a3239b8dc103d
SUPERBOX\Yusuf Umar Hanafi <flyme.yusuf@gmail.com>
2018-05-24 20:58:22 +0700

    Initial commit

---
+++ app/src/main/java/com/sentraup/railmo/railmotrack/MainActivity.java
@@ -0,0 +1,310 @@
+package com.sentraup.railmo.railmotrack;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.sentraup.railmo.railmotrack.app.AppController;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback
+        , GoogleMap.OnMyLocationButtonClickListener
+        , GoogleMap.OnMyLocationClickListener, LocationListener {
+
+    GoogleMap gMap;
+    MarkerOptions markerOptions = new MarkerOptions();
+    CameraPosition cameraPosition;
+    LatLng center, latLng;
+    String title;
+    TextView text;
+    double glat;
+    double glng;
+
+    public static double myLat = 0;
+    public static double myLng = 0;
+    public static final String LAT = "lat";
+    public static final String LNG = "lon";
+    //public static final String INDEK = "indek";
+    public static final String TITLE = "vmak";
+    //public static final String JARAK = "jarak";
+    private String URL = "http://railmo.sentraup.com/parsing.php?lat=-7.59669025&lon=112.77421500&rute=1";
+    String tag_json_obj = "json_obj_req";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+
+        getMarkers(glat,glng);
+
+        // Add a marker in Sydney and move the camera
+        /*LatLng sydney = new LatLng(-34, 151);
+        gMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                        , Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            } else {
+                gMap.setMyLocationEnabled(true);
+            }
+        } else {
+            gMap.setMyLocationEnabled(true);
+        }
+
+        gMap.setOnMyLocationButtonClickListener(this);
+        gMap.setOnMyLocationClickListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    gMap.setMyLocationEnabled(true);
+                }
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "My Location Button Clicked", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Lokasiku saat ini : " + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+        gMap.addMarker(new MarkerOptions().position(latLng));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        gMap.animateCamera(CameraUpdateFactory.zoomTo(21));
+
+        LatLng latLngToAppearMarker;
+
+        latLngToAppearMarker = getMarkers(latitude,longitude);
+
+        gMap.clear();
+        gMap.addMarker(new MarkerOptions().position(latLngToAppearMarker));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    private void addMarker(LatLng latlng, final String title) {
+        markerOptions.position(latlng);
+        markerOptions.title(title);
+        gMap.addMarker(markerOptions);
+
+        gMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Toast.makeText(getApplicationContext(), marker.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // Fungsi get JSON marker
+    private LatLng getMarkers(double lat, double lng) {
+        String requestaddress = "http://railmo.sentraup.com/parsing.php?lat="+lat+"&lon="+lng+"&rute=1";
+        StringRequest strReq = new StringRequest(Request.Method.POST, requestaddress, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.e("Response: ", response.toString());
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    String getObject = jObj.getString("node");
+                    JSONArray jsonArray = new JSONArray(getObject);
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        title = "Kecepatan Maksimal :" + jsonObject.getString(TITLE);
+                        latLng = new LatLng(Double.parseDouble(jsonObject.getString(LAT)), Double.parseDouble(jsonObject.getString(LNG)));
+
+                        // Menambah data marker untuk di tampilkan ke google map
+                        addMarker(latLng, title);
+                    }
+
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error: ", error.getMessage());
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(strReq, tag_json_obj);
+        return  latLng;
+    }
+}
+
+/*public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    MapFragment mapFragment;
+    GoogleMap gMap;
+    MarkerOptions markerOptions = new MarkerOptions();
+    CameraPosition cameraPosition;
+    LatLng center, latLng;
+    String title;
+    TextView text;
+
+    public static final String LAT = "lat";
+    public static final String LNG = "lon";
+    //public static final String INDEK = "indek";
+    public static final String TITLE = "vmak";
+    //public static final String JARAK = "jarak";
+    private String URL = "http://railmo.sentraup.com/parsing.php?lat=-7.59669025&lon=112.77421500&rute=1";
+    String tag_json_obj = "json_obj_req";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+
+        center = new LatLng(-7.59669025, 112.77421500);
+        cameraPosition = new CameraPosition.Builder().target(center).zoom(10).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        getMarkers();
+    }
+
+    private void addMarker(LatLng latlng, final String title) {
+        markerOptions.position(latlng);
+        markerOptions.title(title);
+        gMap.addMarker(markerOptions);
+
+        gMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Toast.makeText(getApplicationContext(), marker.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // Fungsi get JSON marker
+    private void getMarkers() {
+        StringRequest strReq = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.e("Response: ", response.toString());
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    String getObject = jObj.getString("node");
+                    JSONArray jsonArray = new JSONArray(getObject);
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        title = jsonObject.getString(TITLE);
+                        latLng = new LatLng(Double.parseDouble(jsonObject.getString(LAT)), Double.parseDouble(jsonObject.getString(LNG)));
+
+                        // Menambah data marker untuk di tampilkan ke google map
+                        addMarker(latLng, title);
+                    }
+
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error: ", error.getMessage());
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(strReq, tag_json_obj);
+    }
+
+} */
